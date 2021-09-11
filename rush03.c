b2ec7fc1f0499475177e57ceb3a9d3ed0b78728b
Issaka hama barhamou <hamabarhamou@gmail.com>
2021-09-11 12:18:49 +0100

    Finalisation Roche-00

--- rush03.c
+++ rush03.c
@@ -6,22 +6,24 @@
 /*   By: bissaka- <bissaka-@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/10 18:52:26 by bissaka-          #+#    #+#             */
-/*   Updated: 2021/09/11 10:13:55 by bissaka-         ###   ########.fr       */
+/*   Updated: 2021/09/11 12:14:27 by bissaka-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include<unistd.h>
 
+#include "ft_putchar.c"
+
 void	affiche(int hi, int lj, int haut, int larg)
 {
-	if ((((hi == 0) || ((hi == haut))) && (lj == 0)))
-		ft_putchar("A");
-	else if (((hi == 0) || (hi == haut)) && (lj == larg))
-		ft_putchar("C");
-	else if ((hi != 0) && (hi != haut) && (lj != 0) && (lj != larg))
-		ft_putchar(" ");
+	if ((((hi == 0) || ((hi == haut-1))) && (lj == 0)))
+		ft_putchar('A');
+	else if (((hi == 0) || (hi == haut-1)) && (lj == larg-1))
+		ft_putchar('C');
+	else if ((hi != 0) && (hi != haut-1) && (lj != 0) && (lj != larg-1))
+		ft_putchar(' ');
 	else
-		ft_putchar("B");
+		ft_putchar('B');
 }
 
 void	rush(int x, int y)
@@ -31,11 +33,11 @@ void	rush(int x, int y)
 
 	i = 0;
 	j = 0;
-	while (i <= x)
+	while (i < y)
 	{
-		while (j <= y)
+		while (j < x)
 		{
-			affiche(i, j, x, y);
+			affiche(i, j, y, x);
 			j++;
 		}
 		write(1, "\n", 1);
